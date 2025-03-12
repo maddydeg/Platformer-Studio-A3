26b41a96369aeaea948651c438929fcfb94a6788
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-03-11 19:49:26 -0700

    Implemented full animated coin counter and adjusted physics so that player can move.

--- Assets/_Scripts/UI/CoinCounterUI.cs
+++ Assets/_Scripts/UI/CoinCounterUI.cs
@@ -9,6 +9,7 @@ public class CoinCounterUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI toUpdate;
     [SerializeField] private Transform coinTextContainer;
     [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
 
     private float containerInitPosition;
     private float moveAmount;
@@ -28,6 +29,19 @@ public class CoinCounterUI : MonoBehaviour
         toUpdate.SetText($"{score}");
 
         //trigger the local move animation
-        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+        //adding .SetEase(animationCurve) lets us use different animation curves for the dotweenanimation
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
+    }
+
+    private IEnumerator ResetCoinContainer(int score)
+    {
+        //this tells the editor to wait for the duration before executing the next line of code
+        yield return new WaitForSeconds(duration);
+        //we use duration since that's the same time as the animation
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+        //then reset the y-localPosition of the coinTextContainer
     }
 }
